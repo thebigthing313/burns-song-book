758c2acdfee0eb82ad0c4f157b5fd69b7293aa94
thebigthing313 <adriankabigting@gmail.com>
2025-10-19 09:12:01 -0400

    refactor: update App component layout by adding SearchBar and adjusting styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/header';
 import { MainContent } from './components/main-content';
+import { SearchBar } from './components/search-bar';
 
 function App() {
   return (
-    <div className='flex flex-col w-screen h-screen'>
-      <Header className='bg-accent w-full flex flex-row justify-between border-b p-6  items-center' />
-      <MainContent className='bg-background flex-1 p-6 overflow-y-auto' />
+    <div className='flex flex-col gap-2 w-screen h-screen'>
+      <Header className='bg-accent w-full flex flex-row justify-between border-b px-6 py-2 items-center' />
+      <SearchBar className='px-6 py-2' />
+      <MainContent className='bg-background flex-1 px-6 overflow-y-auto' />
     </div>
   );
 }
